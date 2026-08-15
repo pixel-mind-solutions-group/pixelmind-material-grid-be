86791f42ed874969054cd4bcef2530a93eb24e35
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2026-08-15 07:08:37 +0530

    inital commit

---
+++ src/main/java/com/pixelmind/materialgrid/MaterialgridApplication.java
@@ -0,0 +1,13 @@
+package com.pixelmind.materialgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialgridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixelmind/materialgrid/MaterialgridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelmind.materialgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialgridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
